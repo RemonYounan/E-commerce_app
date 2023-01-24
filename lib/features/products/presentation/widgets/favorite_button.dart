a8f162eafa9858a4aa8368a3ba241eaee8c26e2f
RemonYounan <reemooyounan@gmail.com>
2023-01-24 18:56:41 +0200

    added favorite button animation

--- lib/features/products/presentation/widgets/favorite_button.dart
+++ lib/features/products/presentation/widgets/favorite_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:like_button/like_button.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../core/common/app_colors.dart';
@@ -17,47 +18,40 @@ class FavoriteButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthCubit, AuthState>(
-      builder: (context, state) {
-        if (state is AuthSuccessState) {
-          final userId = state.user.id;
-          bool isFav =
-              Provider.of<GlobalProvider>(context).favProducts['$id'] == true;
-          return GestureDetector(
-            onTap: () {
-              Provider.of<GlobalProvider>(context, listen: false)
-                  .toggleFavorite(id, userId);
-            },
-            child: Container(
-              height: 40.r,
-              width: 40.r,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).brightness == Brightness.dark
-                      ? AppColors.dark
-                      : AppColors.white,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 4),
-                      blurRadius: 8,
-                      color: AppColors.shadowColor,
-                    ),
-                  ]),
-              child: Icon(
-                isFav ? Icons.favorite : Icons.favorite_outline_rounded,
-                color: isFav
-                    ? AppColors.primaryColor
-                    : (Theme.of(context).brightness == Brightness.dark
-                        ? AppColors.greyDark
-                        : AppColors.grey),
-                size: 22.r,
-              ),
+    final userId = BlocProvider.of<AuthCubit>(context).user.id;
+    bool isFav =
+        Provider.of<GlobalProvider>(context).favProducts['$id'] == true;
+    return Container(
+      height: 40.r,
+      width: 40.r,
+      decoration: BoxDecoration(
+          color: Theme.of(context).brightness == Brightness.dark
+              ? AppColors.dark
+              : AppColors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 4),
+              blurRadius: 8,
+              color: AppColors.shadowColor,
             ),
-          );
-        } else {
-          return const SizedBox.shrink();
-        }
-      },
+          ]),
+      child: Center(
+        child: LikeButton(
+          isLiked: isFav,
+          size: 25.r,
+          likeBuilder: (isLiked) {
+            final icon = isLiked ? Icons.favorite : Icons.favorite_border;
+            final color = isLiked ? Colors.red : AppColors.grey;
+            return Icon(icon, color: color, size: 25.r);
+          },
+          onTap: (isLiked) async {
+            Provider.of<GlobalProvider>(context, listen: false)
+                .toggleFavorite(id, userId);
+            return !isLiked;
+          },
+        ),
+      ),
     );
   }
 }
