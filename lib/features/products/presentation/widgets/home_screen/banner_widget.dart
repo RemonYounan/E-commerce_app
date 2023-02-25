9ffb0501fe9df7e773c8d04450a90f2de6585964
RemonYounan <reemooyounan@gmail.com>
2023-02-25 21:04:04 +0200

    added smooth page indicator for banner slider

--- lib/features/products/presentation/widgets/home_screen/banner_widget.dart
+++ lib/features/products/presentation/widgets/home_screen/banner_widget.dart
@@ -1,8 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_app/core/utils/error_message_wiget.dart';
-import 'package:ecommerce_app/core/utils/loading_widget.dart';
 import 'package:ecommerce_app/core/utils/placeholder_loading_widget.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../../../core/common/app_colors.dart';
 import '../../blocs/products_cubit/products_cubit.dart';
@@ -21,15 +21,10 @@ class BannerWidget extends StatefulWidget {
 
 class _BannerWidgetState extends State<BannerWidget> {
   int _currentIndex = 0;
-  late CarouselController _controller;
-  @override
-  void initState() {
-    super.initState();
-    _controller = CarouselController();
-  }
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
     return BlocBuilder<ProductsCubit, ProductsState>(
       builder: (context, state) {
         if (state.status == ProductsStatus.loading) {
@@ -47,7 +42,6 @@ class _BannerWidgetState extends State<BannerWidget> {
                 Expanded(
                   flex: 1,
                   child: CarouselSlider.builder(
-                    carouselController: _controller,
                     itemCount: banners.length,
                     itemBuilder: (context, index, realIndex) => Stack(
                       children: [
@@ -83,6 +77,7 @@ class _BannerWidgetState extends State<BannerWidget> {
                     options: CarouselOptions(
                       autoPlay: true,
                       enlargeCenterPage: true,
+                      enlargeFactor: 0.45,
                       viewportFraction: 1,
                       onPageChanged: (i, _) {
                         setState(() {
@@ -92,26 +87,16 @@ class _BannerWidgetState extends State<BannerWidget> {
                     ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    banners.length,
-                    (index) => GestureDetector(
-                      onTap: () => _controller.animateToPage(
-                        index,
-                        curve: Curves.easeInOut,
-                      ),
-                      child: Container(
-                        width: 12.r,
-                        height: 12.r,
-                        margin: EdgeInsets.symmetric(
-                            vertical: 8.h, horizontal: 4.w),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: (Theme.of(context).primaryColor).withOpacity(
-                                _currentIndex == index ? 0.9 : 0.2)),
-                      ),
-                    ),
+                AnimatedSmoothIndicator(
+                  activeIndex: _currentIndex,
+                  count: banners.length,
+                  effect: WormEffect(
+                    dotWidth: 12.r,
+                    dotHeight: 12.r,
+                    dotColor: isDark ? AppColors.greyDark : AppColors.grey,
+                    activeDotColor: isDark
+                        ? AppColors.primaryColorDark
+                        : AppColors.primaryColor,
                   ),
                 ),
               ],
